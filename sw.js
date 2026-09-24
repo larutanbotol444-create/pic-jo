e393ba364895ab061ad6c3e881a593847789b4d9
larutanbotol444-create <larutanbotol444@gmail.com>
2026-09-24 17:56:08 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,15 @@
-const C='picjo-v1',F=['./','index.html','manifest.webmanifest','icon-192.png','icon-512.png'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(F)));self.skipWaiting()});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))));self.clients.claim()});
-self.addEventListener('fetch',e=>{const r=e.request;if(r.method!=='GET')return;
-const save=n=>{const c=n.clone();caches.open(C).then(x=>x.put(r,c));return n};
-e.respondWith(r.mode==='navigate'?fetch(r).then(save).catch(()=>caches.match('index.html')):caches.match(r).then(m=>m||fetch(r).then(save)))});
+const C='picjo',F=['./','index.html','manifest.webmanifest','icon-192.png','icon-512.png'];
+self.addEventListener('install',e=>{
+ e.waitUntil(caches.open(C).then(c=>Promise.all(F.map(u=>fetch(u,{cache:'no-store'}).then(r=>{if(r&&r.ok)return c.put(u,r)}).catch(()=>{})))));
+ self.skipWaiting();
+});
+self.addEventListener('activate',e=>{e.waitUntil(self.clients.claim())});
+self.addEventListener('fetch',e=>{
+ const r=e.request;if(r.method!=='GET')return;
+ e.respondWith(
+  fetch(r,{cache:'no-store'}).then(res=>{
+   if(res&&res.ok){const c=res.clone();caches.open(C).then(x=>x.put(r,c))}
+   return res;
+  }).catch(()=>caches.match(r).then(m=>m||(r.mode==='navigate'?caches.match('index.html'):new Response('',{status:504}))))
+ );
+});
